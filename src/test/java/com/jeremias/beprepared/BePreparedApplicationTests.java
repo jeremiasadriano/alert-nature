4362f552390f2473b4a451359735af6835d4c4f6
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-07-16 23:27:53 +0200

    Init of project

---
+++ src/test/java/com/jeremias/beprepared/BePreparedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremias.beprepared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePreparedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
