b16c66f4bca84376e56322045169a0caa194e61f
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-08-07 18:07:07 +0200

    Security side done

--- src/main/java/com/jeremias/beprepared/security/JwtAuthenticationFilter.java
+++ src/main/java/com/jeremias/beprepared/security/JwtAuthenticationFilter.java
@@ -1,18 +1,22 @@
 package com.jeremias.beprepared.security;
 
+import com.jeremias.beprepared.exceptions.handlers.EntityBadRequestException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Optional;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -21,14 +25,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        if (extractToken(request).isEmpty()) {
+            filterChain.doFilter(request, response);
+            throw new EntityBadRequestException("The token cannot be null");
+        }
+        String token = extractToken(request).get();
+        String username = this.jwtService.getUsername(token);
+        if (this.jwtService.isValid(token, username)) {
+            UserDetails userDetails = authDetailsServiceImpls.loadUserByUsername(username);
+            var auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
     }
 
     private Optional<String> extractToken(HttpServletRequest request) {
-        try {
-            String token = request.getHeader("Authorization");
-        } catch (Exception e) {
-            log.error("Error extracting the token", e);
+        String tokenPrefix = "Bearer ";
+        String token = request.getHeader("Authorization");
+        if (StringUtils.hasText(token) && token.startsWith(tokenPrefix)) {
+            return Optional.of(token.replace(tokenPrefix, ""));
         }
+        return Optional.empty();
     }
 }
---
+++ src/main/java/com/jeremias/beprepared/security/WebSecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.jeremias.beprepared.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+    private final AuthDetailsServiceImpls authDetailsServiceImpls;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.
+                        requestMatchers(HttpMethod.POST, "**/login/**", "**/register/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(authDetailsServiceImpls)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
